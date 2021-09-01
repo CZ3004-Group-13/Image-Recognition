16e7830d1616d49c64d0b3cf80e5af6cb7182370
Tan Jun Hong <junhong.tan98@gmail.com>
2021-09-01 22:44:34 +0800

    Update Photo_Taking_Helper.py

--- Photo_Taking_Helper.py
+++ Photo_Taking_Helper.py
@@ -1,8 +1,14 @@
+# Before running, please change to the correct directory!
+import os
+import time
 from picamera import PiCamera
 camera = PiCamera()
+
+# Change this ID every single time!
 id = '0'
-os.mkdir(id.zfill(2))
+
 for i in range(20):
-    camera.capture(id.zfill(2) + '/image' + id + '-' + str(i).zfill(2) + '.jpg')
+    camera.capture('image' + id + '-' + str(i).zfill(2) + '.jpg')
     time.sleep(0.5)
+print('Done!')
 	
