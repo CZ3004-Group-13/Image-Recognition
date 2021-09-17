35fc87b6b43b2632ee497efa8877a2715a17fb76
Tan Jun Hong <junhong.tan98@gmail.com>
2021-09-18 00:53:40 +0800

    Fix bug

--- Train_Test_Split.py
+++ Train_Test_Split.py
@@ -14,18 +14,19 @@ for subdir, dirs, files in os.walk(".\\ts"):
                 prev = file.split('e')[1].split('-')[0]
                 print(prev)
                 tempList = ['data/ts/' + file]
+    lst.append(tempList)
                
 for i in lst:
     random.shuffle(i)
     
 f = open("test.txt", "w")
 for i in range(len(lst)):
-    for j in range(5):
+    for j in range((int) (len(lst[i]) / 5)):
         f.write(lst[i][j] + '\n')
 f.close()
 
 f = open("train.txt", "w")
 for i in range(len(lst)):
-    for j in range(5, len(lst[i])):
+    for j in range((int) (len(lst[i]) / 5), len(lst[i])):
         f.write(lst[i][j] + '\n')
 f.close()
