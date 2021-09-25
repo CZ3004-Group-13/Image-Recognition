30db428f94b7779150e1cbb2049e3fe23e9dc75a
Tan Jun Hong <junhong.tan98@gmail.com>
2021-09-25 11:45:22 +0800

    Update Train_Test_Split.py

--- Train_Test_Split.py
+++ Train_Test_Split.py
@@ -1,32 +1,9 @@
-import os
-import random
-
-lst = []
-prev = "0"
-for subdir, dirs, files in os.walk(".\\ts"):
-    tempList = []
-    for file in files:
-        if (file.endswith('.jpg')):
-            if (file.startswith('image' + prev + '-')):
-                tempList.append('data/ts/' + file)
-            else:
-                lst.append(tempList)
-                prev = file.split('e')[1].split('-')[0]
-                print(prev)
-                tempList = ['data/ts/' + file]
-    lst.append(tempList)
-               
-for i in lst:
-    random.shuffle(i)
-    
 f = open("test.txt", "w")
-for i in range(len(lst)):
-    for j in range((int) (len(lst[i]) / 5)):
-        f.write(lst[i][j] + '\n')
+for i in range(0, 1546, 2):
+    f.write("data/ts/" + str(i) + ".jpg\n")
 f.close()
 
 f = open("train.txt", "w")
-for i in range(len(lst)):
-    for j in range((int) (len(lst[i]) / 5), len(lst[i])):
-        f.write(lst[i][j] + '\n')
+for i in range(1, 1546, 2):
+    f.write("data/ts/" + str(i) + ".jpg\n")
 f.close()
